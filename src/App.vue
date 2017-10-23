ca55a6c8b615207767e3d46653e714923a31ff0c
AvA4EvR <31218871+AvA4EvR@users.noreply.github.com>
2017-10-22 22:07:34 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="container">
+        <!--ToDo: Use the Header component selector-->
+        <appHeader>
+            <!--Bind data props quoteCount and set it equal to the quote length-->
+            <!--Bind data props maxQuotes and set it equal to the maxQuotes created below-->
+        </appHeader>
+
+
+        <!--ToDo: Use the NewQuote component selector-->
+        <appNewQuote>
+            <!--Initiate an $emit event listener called quoteAdded that is equal to newQuote-->
+        </appNewQuote>
+
+        <!--ToDo: Use the QuoteGrid component selector-->
+        <appQuoteGrid>
+            <!--Bind data props quotes and set it equal to the quotes array created below-->
+            <!--Call quoteDeleted that is emitted from the QuoteGrid Component and pass the deleteQuote method created below-->
+        </appQuoteGrid>
+
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+//  Todo: Import QuoteGrid, NewQuote, and Header Components ()
+    import Header from './components/Header.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import QuoteGrid from './components/QuoteGrid.vue';
+
+    export default {
+        data: function () {
+            return {
+                // ToDo: Create an array data object called quotes
+                quotes: ['string'],
+                    // Have at least one string quote by default
+
+                // ToDo: Create a data object maxQuotes that is set to 10
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            // ToDo: Create a method that checks when a newQuote is created and takes the parameter quote.
+            newQuote(quote){
+                if (this.quotes.length >= this.maxQuotes){
+                    return alert("Delete a quote")
+                }
+                this.quotes.push(quote);
+            },
+                // This method will check if the quotes (Array Data Object) length is greater than or equal to the max quotes allowed
+                // If it is return an alert telling the user to delete a quote
+                // Outside of conditional, push quote (method parameter) to the quotes array
+
+            // ToDo: Create a method deleteQuote that takes in a parameter of index
+                // This method will splice the quotes array by (index, 1)
+            deleteQuote(index) {
+                index.splice(index, 1);
+            }
+        },
+        components: {
+            // ToDo: Initiate the QuoteGrid, NewQuote, and Header Components with Custom Selectors
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header
+        }
+        
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="row">
+        <!--ToDo: Use the Quote component selector-->
+        <appQuote v-for="Quotes in Quote"
+        :Quote="Quote" @click.native="deleteQuote">
+
+
+            <!--Loop through the quote and index in quotes-->
+
+            <!--Add a click event with .native and set it equal to the deleteQuote method created below-->
+            <!--Display the quote inside the appQuote component, in other words pass the data object to the slot in Quote.vue-->
+        </appQuote>
+    </div>
+</template>
+
+<script>
+    // ToDo: import the Quote Component
+    import Quote from './Quote.vue'
+
+    export default {
+        // ToDo: Create props of quotes
+        props: ['quotes'],
+
+        components: {
+            // ToDo: Initiate the Quote Component with a Custom Selector
+            appQuote: Quote
+        },
+        methods: {
+            // ToDo: Create a method deleteQuote that takes a paremeter of index
+                // Emit and event listener named quotesDeleted and passes the index parameter
+            deleteQuote(index){
+                eventBus.$on('quotesDeleted', (index) => {
+
+                })
+            }
+        }
+    }
+</script>
